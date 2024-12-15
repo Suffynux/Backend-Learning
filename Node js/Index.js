7738c7e324a09e2db87e07747790a82bd8e9771f
Sufiyan <suffynux@gmail.com>
2024-12-15 10:54:46 +0500

    Module imported

--- Node js/Index.js
+++ Node js/Index.js
@@ -1,2 +1,11 @@
 // importing File struture module
 const fs = require('fs');
+const filePath = "./Tasks.json";
+
+// Command for user Atteraction to the application
+
+if(command === 'add'){
+    addTask()
+} else if (command === 'list') {
+    listTasks()
+} else if ( )
